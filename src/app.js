4103687eb6aa0abe6e76beb71ab1dce1430e55cf
Shandeep Singh <shandeepsingh96@gmail.com>
2019-10-30 19:55:04 -0400

    refactored

--- src/app.js
+++ src/app.js
@@ -1,63 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routes/AppRouter';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-const BudgetDashboardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is from my add expense component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense page
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is from my help page
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404 PAGE - <Link to="/">Go Home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Budgeter</h1>
-        <NavLink to="/" activeClassName="is-active" exact>Home</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-);
-
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" component={BudgetDashboardPage} exact />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/routes/AppRouter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import BudgetDashboardPage from '../components/BudgetDashboardPageComponent';
+import AddExpensePage from '../components/AddExpensePageComponent';
+import EditExpensePage from '../components/EditExpensePageComponent';
+import HelpPage from '../components/HelpPageComponent';
+import NotFoundPage from '../components/NotFoundPageComponent';
+import Header from '../components/HeaderComponent';
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={BudgetDashboardPage} exact />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;
