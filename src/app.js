09264e4dbec8851c28cdd9f18c4f959d35968a58
Shandeep Singh <shandeepsingh96@gmail.com>
2019-10-30 16:57:40 -0400

    setup routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Budgeter</p>, document.getElementById('app'));
+const BudgetDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense page
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my help page
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        404 PAGE
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={BudgetDashboardPage} exact />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
