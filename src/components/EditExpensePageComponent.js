80b0b5b43de9714bf5b9850c00bdcf3a86238029
Shandeep Singh <shandeepsingh96@gmail.com>
2019-10-30 20:52:30 -0400

    refactored

--- src/components/EditExpensePageComponent.js
+++ src/components/EditExpensePageComponent.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense page
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props)
+    return (
+        <div>
+            This is from my edit expense page {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpensePage;
