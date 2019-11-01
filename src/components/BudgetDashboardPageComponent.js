1898d2637b67149e78a71760ae187e82dc51aa63
Shandeep Singh <shandeepsingh96@gmail.com>
2019-11-01 15:56:36 -0400

    create remove button

--- src/components/BudgetDashboardPageComponent.js
+++ src/components/BudgetDashboardPageComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const BudgetDashboardPage = () => (
     <div>
-        <ExpenseList/>
+        <ExpenseListFilters />
+        <ExpenseList />
     </div>
 );
 
