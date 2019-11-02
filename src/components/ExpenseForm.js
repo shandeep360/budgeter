a7b6f6574071b7adc5c7d59a134178457e107720
Shandeep Singh <shandeepsingh96@gmail.com>
2019-11-02 12:37:03 -0400

    createLTR error fix

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,29 +1,42 @@
 import React from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+// const date = new Date();
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
     };
-
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({ description }));
     };
-
     onNoteChange = (e) => {
         const note = e.target.value;
         this.setState(() => ({ note }));
     };
-
     onAmountChange = (e) => {
         const amount = e.target.value;
+
         if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
     };
-
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    };
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }));
+    };
     render() {
         return (
             <div>
@@ -41,8 +54,16 @@ export default class ExpenseForm extends React.Component {
                         value={this.state.amount}
                         onChange={this.onAmountChange}
                     />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                    />
                     <textarea
-                        placeholder="Notes (optional)"
+                        placeholder="Add a note (optional)"
                         value={this.state.note}
                         onChange={this.onNoteChange}
                     >
@@ -50,6 +71,6 @@ export default class ExpenseForm extends React.Component {
                     <button>Add Expense</button>
                 </form>
             </div>
-        );
-    };
-};
+        )
+    }
+}
